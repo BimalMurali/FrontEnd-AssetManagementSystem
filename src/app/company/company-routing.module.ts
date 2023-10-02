16158538eefc2c1999e408586539fdc8745afd78
anjanac <anjanac.janardhanan@gmail.com>
2023-10-02 21:56:35 +0530

    Company Home Page added

--- src/app/company/company-routing.module.ts
+++ src/app/company/company-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {VendorListComponent} from './vendor-list/vendor-list.component'
+import { HomecompanyComponent } from './homecompany/homecompany.component';
 
 const routes: Routes = [
-  {path:'vendorlist',component:VendorListComponent}
+  {path:'vendorlist',component:VendorListComponent},
+  {path:'homecompany',component:HomecompanyComponent}
 ];
 
 @NgModule({
