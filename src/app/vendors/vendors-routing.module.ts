e51d65ce5316fed1f549c34df0f222d6b65fc21f
BimalMurali <bimalmurali999@gmail.com>
2023-10-03 10:33:40 +0530

    vendor routing added

--- src/app/vendors/vendors-routing.module.ts
+++ src/app/vendors/vendors-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomevendorsComponent } from './homevendors/homevendors.component';
+import {ListassetsComponent} from './listassets/listassets.component'
+import {EditassetsComponent} from './editassets/editassets.component'
+import {AddassetsComponent} from './addassets/addassets.component'
+import {UpdatestatusComponent} from './updatestatus/updatestatus.component'
 
 const routes: Routes = [
-  {path:'home',component:HomevendorsComponent}
+  {path:'home',component:HomevendorsComponent},
+  {path :'listassets',component:ListassetsComponent},
+  {path :'editassets',component:EditassetsComponent},
+  {path :'addassets',component:AddassetsComponent},
+  {path :'updatestatus',component:UpdatestatusComponent},
 ];
 
 @NgModule({
