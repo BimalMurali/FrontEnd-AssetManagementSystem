fd73d74c92c92d7ea13ea27f84043ba09d95eca3
BimalMurali <bimalmurali999@gmail.com>
2023-10-03 18:17:30 +0530

    asset creation list added

--- src/app/assetcreation/assetcreation-routing.module.ts
+++ src/app/assetcreation/assetcreation-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AssetcreationListComponent } from './assetcreation-list/assetcreation-list.component';
+import { AssetcreationEditComponent } from './assetcreation-edit/assetcreation-edit.component';
+import { AssetcreationAddComponent } from './assetcreation-add/assetcreation-add.component';
+import { AssetcreationDeleteComponent } from './assetcreation-delete/assetcreation-delete.component';
 
 const routes: Routes = [
   {path:'list',component:AssetcreationListComponent},
+  {path:'edit',component:AssetcreationEditComponent},
+  {path:'add',component:AssetcreationAddComponent},
+  {path:'delete',component:AssetcreationDeleteComponent},
 
 ];
 
