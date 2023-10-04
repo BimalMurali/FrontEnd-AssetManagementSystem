78ae5753b6bb64686cab94150a3c29c5032f44dd
anjanac <anjanac.janardhanan@gmail.com>
2023-10-05 00:25:35 +0530

    is active added

--- src/app/vendordetails/vendorlist/vendorlist.component.ts
+++ src/app/vendordetails/vendorlist/vendorlist.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Vendors } from 'src/app/shared/models/vendors';
 import { VendordetailsService } from 'src/app/shared/services/vendordetails.service'
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-vendorlist',
   templateUrl: './vendorlist.component.html',
@@ -16,6 +18,8 @@ export class VendorlistComponent implements OnInit {
   constructor(public vendordetailsService: VendordetailsService,
     private router: Router) { }
 
+    private vendorDataSubscription: Subscription;
+    
   ngOnInit(): void {
     console.log(this.vendordetailsService.getAllVendors());
     this.vendordetailsService.getAllVendors();
@@ -28,6 +32,7 @@ export class VendorlistComponent implements OnInit {
 
     this.populateVendorData(vendors)
     this.router.navigate(['vendordetails/edit',vendors.id])
+    this.vendorDataSubscription.unsubscribe();
     // local host:4200/employees/edit/id
   }
   //getting vendor data
@@ -35,5 +40,19 @@ export class VendorlistComponent implements OnInit {
     console.log(vendors.validFrom+"in list");
     this.vendordetailsService.formVendorData=Object.assign({},vendors);
   }
+  disableOrder(_id:number){
+    if(confirm("Do you want to delete this record?")){
+      this.vendordetailsService.disableOrder(_id)
+      .subscribe(
+        (response)=>{
+          console.log(response);
+          this.vendordetailsService.getAllVendors();
+        },
+        (error)=>{
+        console.log(error);
+      })
 
 }
+  }
+}
+
